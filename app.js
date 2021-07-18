d605cbaaa8cdbf421a5e5d59487f2b3601dfcccc
king <stansgift@gmail.com>
2021-07-18 17:49:14 -0400

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello universe!'));
+app.get('/', (req, res) => res.send('today is sunday!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
